fd9341e21d6309aaaa731f962862b804de43872b
Shubham Deep <shubham.deep@go-mmt.com>
2023-02-01 15:30:01 +0530

    Encryption added

--- Dao/EmployeeDao.go
+++ Dao/EmployeeDao.go
@@ -3,8 +3,10 @@ package dao
 import (
 	"rest/gin/Config"
 	"rest/gin/models"
-   "strconv"
+	"strconv"
+
 	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type EmployeeDao interface {
@@ -30,8 +32,12 @@ func (ed *EmployeeDaoImpl) GetAllEmployees() ([]*models.Employee, error) {
 
 }
 
-func (ed *EmployeeDaoImpl) CreateEmployees(employee *models.CreateEmployee) (err error) {
-
+func (ed *EmployeeDaoImpl) CreateEmployees(employee *models.CreateEmployee) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(employee.Password), 8)
+	if err != nil {
+		return err
+	}
+	employee.Password = string(hashedPassword)
 	if err = Config.DB.Create(&employee).Error; err != nil {
 		return err
 	}
@@ -48,19 +54,18 @@ func (ed *EmployeeDaoImpl) GetEmployeeById(id string) (*models.Employee, error) 
 }
 
 func (ed *EmployeeDaoImpl) UpdateEmployee(employee *models.CreateEmployee, id string) (err error) {
-	empid,_:=strconv.ParseInt(id, 10, 32)
-     employeeToEdit:=models.Employee{
-        ID:empid,
-		Name:employee.Name,
-		Mobile:employee.Mobile,
-        Address:employee.Address,
-		Age:employee.Age,
-		Salary:employee.Salary,
-        Username:employee.Username,
-		Password:employee.Password,
-    }
-	 
-	
+	empid, _ := strconv.ParseInt(id, 10, 32)
+	employeeToEdit := models.Employee{
+		ID:       empid,
+		Name:     employee.Name,
+		Mobile:   employee.Mobile,
+		Address:  employee.Address,
+		Age:      employee.Age,
+		Salary:   employee.Salary,
+		Username: employee.Username,
+		Password: employee.Password,
+	}
+
 	if err = Config.DB.Save(&employeeToEdit).Error; err != nil {
 		return err
 	}
